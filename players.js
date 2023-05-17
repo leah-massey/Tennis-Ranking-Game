45be84c6199084994789fa8e61e8a1826d830c1a
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-17 15:36:42 +0100

    players.js file updated to be an array of javascript objects.

--- players.js
+++ players.js
@@ -1,106 +1,105 @@
 'use strict';
 
-const player1 = {
-  name: ['Aryna', 'Sabalenka'],
-  ranking: 2,
-  image: 
-};
+const players = [
+  {
+    name: ['Aryna', 'Sabalenka'],
+    ranking: 2,
+  },
 
-const player2 = {
-  name: ['Iga', 'Swiatek'],
-  ranking: 1,
-};
+  {
+    name: ['Iga', 'Swiatek'],
+    ranking: 1,
+  },
 
-const player3 = {
-  name: ['Jessica', 'Pegula'],
-  ranking: 3,
-};
+  {
+    name: ['Jessica', 'Pegula'],
+    ranking: 3,
+  },
 
-const player4 = {
-  name: ['Caroline', 'Garcia'],
-  ranking: 4,
-}
+  {
+    name: ['Caroline', 'Garcia'],
+    ranking: 4,
+  },
 
-const player5 = {
-  name: ['Coco', 'Gauf'],
-  ranking: 5
-}
+  {
+    name: ['Coco', 'Gauf'],
+    ranking: 5,
+  },
 
-const player6 = {
-  name: ['Elena', 'Rybankina'],
-  ranking: 6
-}
+  {
+    name: ['Elena', 'Rybankina'],
+    ranking: 6,
+  },
 
-const player7 = {
-  name: ['Ons', 'Jabeur'],
-  ranking: 7
-}
+  {
+    name: ['Ons', 'Jabeur'],
+    ranking: 7,
+  },
 
+  {
+    name: ['Maria', 'Sakkari'],
+    ranking: 8,
+  },
 
-const player8 = {
-  name: ['Maria', 'Sakkari'],
-  ranking: 8
-}
+  {
+    name: ['Daria', 'Kasatkina'],
+    ranking: 9,
+  },
 
-const player9 = {
-  name: ['Daria', 'Kasatkina'],
-  ranking: 9
-}
+  {
+    name: ['Petra', 'Kvitova'],
+    ranking: 10,
+  },
 
-const player10 = {
-  name: ['Petra', 'Kvitova'],
-  ranking: 10
-}
+  {
+    name: ['Belinda', 'Bencic'],
+    ranking: 11,
+  },
 
-const player11 = {
-  name: ['Belinda', 'Bencic'],
-  ranking: 11
-}
+  {
+    name: ['Veronika', 'Kudermetova'],
+    ranking: 12,
+  },
 
-const player12 = {
-  name: ['Veronika', 'Kudermetova'],
-  ranking: 12
-}
+  {
+    name: ['Barbora', 'Krejcikova'],
+    ranking: 13,
+  },
 
-const player13 = {
-  name: ['Barbora', 'Krejcikova'],
-  ranking: 13
-}
+  {
+    name: ['Karolina', 'Pliskova'],
+    ranking: 14,
+  },
 
-const player14 = {
-  name: ['Karolina', 'Pliskova'],
-  ranking: 14
-}
+  {
+    name: ['Beatriz', 'Haddad Maia'],
+    ranking: 15,
+  },
 
-const player15 = {
-  name: ['Beatriz', 'Haddad Maia'],
-  ranking: 15
-}
+  {
+    name: ['Liudmila', 'Samsonova'],
+    ranking: 16,
+  },
 
-const player16 = {
-  name: ['Liudmila', 'Samsonova'],
-  ranking: 16
-}
+  {
+    name: ['Victoria', 'Azarenka'],
+    ranking: 17,
+  },
 
-const player17 = {
-  name: ['Victoria', 'Azarenka'],
-  ranking: 17
-}
+  {
+    name: ['Martina', 'Trevisan'],
+    ranking: 18,
+  },
 
+  {
+    name: ['Magda', 'Linette'],
+    ranking: 19,
+  },
 
-const player18 = {
-  name: ['Martina', 'Trevisan'],
-  ranking: 18
-}
+  {
+    name: ['Jelena', 'Ostapenko'],
+    ranking: 20,
+  },
+];
 
-const player19 = {
-  name: ['Magda', 'Linette'],
-  ranking: 19
-}
-
-const player20 = {
-  name: ['Jelena', 'Ostapenko'],
-  ranking: 20
-}
-
-
+module.exports = players;
