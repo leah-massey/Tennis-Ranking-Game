530f8aa5c7df32cdf1ee52d8574ae0629724ff1f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-17 09:52:33 +0100

    If/else statement written for possible guesses - too high, too low, not a valid number, not a number, correct number.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const secretNumber = 12;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess || guess > 20 || guess < 1) {
+    document.querySelector('.message').textContent =
+      'You must enter a number between 1 and 20';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent =
+      "They're better than that!";
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent =
+      "Mmm... they're not quite that good";
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      'Bang on! You won a point!';
+  }
+});
