77563862d20147859e3ff26d5e83c511917a4bdd
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-17 15:13:00 +0100

    New file, players.js added, containing javascript objects of each player in the top 20. Name and rank properties.

---
+++ players.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const player1 = {
+  name: ['Aryna', 'Sabalenka'],
+  ranking: 2,
+  image: 
+};
+
+const player2 = {
+  name: ['Iga', 'Swiatek'],
+  ranking: 1,
+};
+
+const player3 = {
+  name: ['Jessica', 'Pegula'],
+  ranking: 3,
+};
+
+const player4 = {
+  name: ['Caroline', 'Garcia'],
+  ranking: 4,
+}
+
+const player5 = {
+  name: ['Coco', 'Gauf'],
+  ranking: 5
+}
+
+const player6 = {
+  name: ['Elena', 'Rybankina'],
+  ranking: 6
+}
+
+const player7 = {
+  name: ['Ons', 'Jabeur'],
+  ranking: 7
+}
+
+
+const player8 = {
+  name: ['Maria', 'Sakkari'],
+  ranking: 8
+}
+
+const player9 = {
+  name: ['Daria', 'Kasatkina'],
+  ranking: 9
+}
+
+const player10 = {
+  name: ['Petra', 'Kvitova'],
+  ranking: 10
+}
+
+const player11 = {
+  name: ['Belinda', 'Bencic'],
+  ranking: 11
+}
+
+const player12 = {
+  name: ['Veronika', 'Kudermetova'],
+  ranking: 12
+}
+
+const player13 = {
+  name: ['Barbora', 'Krejcikova'],
+  ranking: 13
+}
+
+const player14 = {
+  name: ['Karolina', 'Pliskova'],
+  ranking: 14
+}
+
+const player15 = {
+  name: ['Beatriz', 'Haddad Maia'],
+  ranking: 15
+}
+
+const player16 = {
+  name: ['Liudmila', 'Samsonova'],
+  ranking: 16
+}
+
+const player17 = {
+  name: ['Victoria', 'Azarenka'],
+  ranking: 17
+}
+
+
+const player18 = {
+  name: ['Martina', 'Trevisan'],
+  ranking: 18
+}
+
+const player19 = {
+  name: ['Magda', 'Linette'],
+  ranking: 19
+}
+
+const player20 = {
+  name: ['Jelena', 'Ostapenko'],
+  ranking: 20
+}
+
+
--- script.js
+++ script.js
@@ -4,31 +4,34 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
 let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   // guess not valid
   if (!guess || guess > 20 || guess < 1) {
-    document.querySelector('.message').textContent =
-      'You must enter a number between 1 and 20';
+    displayMessage('You must enter a number between 1 and 20');
 
     // guess not correct
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
+      displayMessage(
         guess > secretNumber
           ? 'You underestimate them, try again!'
-          : "Mmm... they're not quite that good";
+          : "Mmm... they're not quite that good"
+      );
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'Sorry, game over';
+      displayMessage('Sorry, game over');
       document.querySelector('.score').textContent = 0;
     }
     // guess correct
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Bang on!';
+    displayMessage('Bang on!');
     if (score > highScore) {
       highScore = score;
     }
