1fb8b234dcd346ec123898fc22182b8a511515af
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-19 19:51:06 +0100

    Logic written to so that 'close' button will close the popup.

--- script.js
+++ script.js
@@ -61,3 +61,8 @@ document.querySelector('.show-modal').addEventListener('click', function () {
   document.querySelector('.modal').classList.remove('hidden');
   document.querySelector('.overlay').classList.remove('hidden');
 });
+
+document.querySelector('.close-modal').addEventListener('click', function () {
+  document.querySelector('.modal').classList.add('hidden');
+  document.querySelector('.overlay').classList.add('hidden');
+});
