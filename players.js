f8c8691301b37aad26bff3c24c9148884420779e
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-06-22 18:54:01 +0100

    Players class created.

--- players.js
+++ players.js
@@ -2,121 +2,161 @@ const players = [
   {
     name: ['Iga', 'Swiatek'],
     ranking: 1,
-    image: './playerImages/player-1',
+    image: {
+      src: './playerImages/player-1',
+    },
   },
 
   {
     name: ['Aryna', 'Sabalenka'],
     ranking: 2,
-    image: './playerImages/player-2',
+    image: {
+      src: './playerImages/player-2',
+    },
   },
 
   {
     name: ['Jessica', 'Pegula'],
     ranking: 3,
-    image: './playerImages/player-3',
+    image: {
+      src: './playerImages/player-3',
+    },
   },
 
   {
     name: ['Elena', 'Rybankina'],
     ranking: 4,
-    image: './playerImages/player-4',
+    image: {
+      src: './playerImages/player-4',
+    },
   },
 
   {
     name: ['Caroline', 'Garcia'],
     ranking: 5,
-    image: './playerImages/player-5',
+    image: {
+      src: './playerImages/player-5',
+    },
   },
 
   {
     name: ['Coco', 'Gauf'],
     ranking: 6,
-    image: './playerImages/player-6',
+    image: {
+      src: './playerImages/player-6',
+    },
   },
 
   {
     name: ['Ons', 'Jabeur'],
     ranking: 7,
-    image: './playerImages/player-7',
+    image: {
+      src: './playerImages/player-7',
+    },
   },
 
   {
     name: ['Maria', 'Sakkari'],
     ranking: 8,
-    image: './playerImages/player-8',
+    image: {
+      src: './playerImages/player-8',
+    },
   },
 
   {
     name: ['Daria', 'Kasatkina'],
     ranking: 9,
-    image: './playerImages/player-9',
+    image: {
+      src: './playerImages/player-9',
+    },
   },
 
   {
     name: ['Petra', 'Kvitova'],
     ranking: 10,
-    image: './playerImages/player-10',
+    image: {
+      src: './playerImages/player-10',
+    },
   },
 
   {
     name: ['Belinda', 'Bencic'],
     ranking: 11,
-    image: './playerImages/player-11',
+    image: {
+      src: './playerImages/player-11',
+    },
   },
 
   {
     name: ['Veronika', 'Kudermetova'],
     ranking: 12,
-    image: './playerImages/player-12',
+    image: {
+      src: './playerImages/player-12',
+    },
   },
 
   {
     name: ['Barbora', 'Krejcikova'],
     ranking: 13,
-    image: './playerImages/player-13',
+    image: {
+      src: './playerImages/player-13',
+    },
   },
 
   {
     name: ['Karolina', 'Pliskova'],
     ranking: 14,
-    image: './playerImages/player-14',
+    image: {
+      src: './playerImages/player-14',
+    },
   },
 
   {
     name: ['Beatriz', 'Haddad Maia'],
     ranking: 15,
-    image: './playerImages/player-15',
+    image: {
+      src: './playerImages/player-15',
+    },
   },
 
   {
     name: ['Liudmila', 'Samsonova'],
     ranking: 16,
-    image: './playerImages/player-16',
+    image: {
+      src: './playerImages/player-16',
+    },
   },
 
   {
     name: ['Qinwen', 'Zheng Vic'],
     ranking: 17,
-    image: './playerImages/player-17',
+    image: {
+      src: './playerImages/player-17',
+    },
   },
 
   {
     name: ['Victoria', 'Azarenka'],
     ranking: 18,
-    image: './playerImages/player-18',
+    image: {
+      src: './playerImages/player-18',
+    },
   },
 
   {
     name: ['Madison', 'Keys'],
     ranking: 19,
-    image: './playerImages/player-19',
+    image: {
+      src: './playerImages/player-19',
+    },
   },
 
   {
     name: ['Jelena', 'Ostapenko'],
     ranking: 20,
-    image: './playerImages/player-20',
+    image: {
+      src: './playerImages/player-20',
+    },
   },
 ];
 
